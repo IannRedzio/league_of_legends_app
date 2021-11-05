801cfe5ce0e9a1ad71c62aae3b843612d6c78dbf
IannRedzio <ianredzio13@gmail.com>
2021-11-05 00:47:26 -0300

    add intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
 import "./App.scss";
 import Header from "./components/header/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
